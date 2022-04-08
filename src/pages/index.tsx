ab1d7fa5bdb9061f41ece090c5fb22263d0b0e37
Felipe dos Anjos <felipe.b.abencoado@gmail.com>
2022-04-08 17:16:16 -0300

    chore: clean project and add typescript

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>World trip</title>
+      </Head>
+      <h1>Hello world</h1>
+    </>
+  )
+}
